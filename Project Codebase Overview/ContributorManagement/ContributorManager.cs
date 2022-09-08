0c74f08ebb80d5c1409c5ce76ad16fc87f4eef16
jacoberiksen1211 <jacob1211@hotmail.com>
2022-09-08 13:58:06 +0200

    ContributorManagement model added

---
+++ Project Codebase Overview/ContributorManagement/ContributorManager.cs
@@ -0,0 +1,54 @@
+﻿using Project_Codebase_Overview.ContributorManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Codebase_Overview.ContributorManagement
+{
+    internal class ContributorManager
+    {
+        Dictionary<string, Author> authors;
+
+        private static ContributorManager instance = null;
+        private int idCounter;
+        private ContributorManager()
+        {
+            authors = new Dictionary<string, Author>();
+            idCounter = 1;
+        }
+
+        public static ContributorManager getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new ContributorManager();
+            }
+            return instance;
+        }
+
+        public void addAuthor(string email, string name)
+        {
+            Author author = new Author(idCounter, email, name);
+            this.authors.Add(email, author);
+            idCounter++;
+        }
+
+        public Author getAuthor (string email)
+        {
+            return this.authors[email];
+        }
+
+        public Author getOrCreateAuthor (string email, string name)
+        {
+            if (!authors.ContainsKey(email))
+            {
+                addAuthor(email, name);
+            }
+            return getAuthor(email);
+        }
+    }
+}
---
+++ Project Codebase Overview/ContributorManagement/Model/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Codebase_Overview.ContributorManagement.Model
+{
+    internal class Author
+    {
+        int id { get; }
+        string name { get; set; }
+        string email { get; set; }
+
+        public Author(int id, string email, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+    }
+}
