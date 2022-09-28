d76497ab5f3b988a6ccd11618e9703b5906d800d
Frederik Schrøder Koefoed <s195463@student.dtu.dk>
2022-09-28 16:56:59 +0200

    Created GraphHelper to simplify creating future graph sections

--- Project Codebase Overview/DataCollection/Model/ExplorerItem.cs
+++ Project Codebase Overview/DataCollection/Model/ExplorerItem.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Project_Codebase_Overview.Graphs;
 using Syncfusion.UI.Xaml.Gauges;
 using System;
 using System.Collections.Generic;
@@ -32,56 +33,18 @@ namespace Project_Codebase_Overview.DataCollection.Model
             SfLinearGauge sfLinearGauge = new SfLinearGauge();
             sfLinearGauge.Axis.Minimum = 0;
             sfLinearGauge.Axis.Maximum = 100;
-            double currentStartPos = 0;
-            var distributionAmount = this.GraphModel.LineDistribution.Count();
-            uint lineCount = 0;
-            var blockCount = 0;
-
-            foreach (var dist in this.GraphModel.LineDistribution.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value))
+            foreach (var block in GraphHelper.GetGraphBlocksFromDistribution(this.GraphModel.LineDistribution, this.GraphModel.LinesTotal))
             {
-                var blockSize = ((double)dist.Value / (double)this.GraphModel.LinesTotal) * 100;
-                if (
-                    blockCount < distributionAmount - 1 &&  //If there is only one block left, just show its color.
-                    (currentStartPos > BAR_GRAPH_CUT_OFF_POINT || // if more blocks after the cut off point, make them one grey block
-                    (currentStartPos > BAR_GRAPH_SMALL_CHECK_STARTING_POINT && blockSize < BAR_GRAPH_SMALL_CHECK_MIN_SIZE)) // if the blocks are very small and after the "small_check_starting_point" make them one grey block
-                )
-                {//Adding "other"-section to graph
-                    LinearGaugeRange gaugeRange = new LinearGaugeRange();
-                    gaugeRange.StartValue = currentStartPos;
-                    gaugeRange.EndValue = 100;
-                    gaugeRange.StartWidth = 25;
-                    gaugeRange.EndWidth = 25;
-                    gaugeRange.Background = new SolidColorBrush(Color.FromArgb(255, 150, 150, 150));
+                LinearGaugeRange gaugeRange = new LinearGaugeRange();
+                gaugeRange.StartValue = block.StartValue;
+                gaugeRange.EndValue = block.EndValue;
+                gaugeRange.StartWidth = 25;
+                gaugeRange.EndWidth = 25;
+                gaugeRange.Background = new SolidColorBrush(block.Color);
 
-                    var msg = string.Format("{0} others: {1:N2}%", distributionAmount-blockCount, (1-(double)lineCount / (double)this.GraphModel.LinesTotal) * 100);
+                ToolTipService.SetToolTip(gaugeRange, block.ToolTip);
 
-                    ToolTipService.SetToolTip(gaugeRange, msg);
-
-                    currentStartPos = gaugeRange.EndValue;
-
-                    sfLinearGauge.Axis.Ranges.Add(gaugeRange);
-                    break;
-                } else
-                {
-                    LinearGaugeRange gaugeRange = new LinearGaugeRange();
-                    gaugeRange.StartValue = currentStartPos;
-                    gaugeRange.EndValue = blockSize + currentStartPos;
-                    gaugeRange.StartWidth = 25;
-                    gaugeRange.EndWidth = 25;
-                    gaugeRange.Background = new SolidColorBrush(dist.Key.Color);
-
-                    var msg = string.Format("{0}: {1:N2}%", dist.Key.Name, blockSize);
-
-                    ToolTipService.SetToolTip(gaugeRange, msg);
-
-                    currentStartPos = gaugeRange.EndValue;
-
-                    lineCount += dist.Value;
-
-                    sfLinearGauge.Axis.Ranges.Add(gaugeRange);
-                }
-                blockCount++;
-                
+                sfLinearGauge.Axis.Ranges.Add(gaugeRange);
             }
             _bargraph = sfLinearGauge;
             return sfLinearGauge;
---
+++ Project Codebase Overview/Graphs/GraphHelper.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Project_Codebase_Overview.ContributorManagement.Model;
+using Project_Codebase_Overview.Graphs.Model;
+using Syncfusion.UI.Xaml.Gauges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace Project_Codebase_Overview.Graphs
+{
+    public class GraphHelper
+    {
+
+        private static readonly int GRAPH_CUT_OFF_POINT = 90;
+        private static readonly int GRAPH_SMALL_CHECK_STARTING_POINT = 60;
+        private static readonly int GRAPH_SMALL_CHECK_MIN_SIZE = 5;
+
+        public static List<GraphBlock> GetGraphBlocksFromDistribution(Dictionary<Author, uint> distributions, uint linesTotal)
+        {
+            var blockList = new List<GraphBlock>();
+            double currentStartPos = 0;
+            var distributionAmount = distributions.Count();
+            uint lineCount = 0;
+            var blockCount = 0;
+
+            foreach (var dist in distributions.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value))
+            {
+                var blockSize = ((double)dist.Value / (double)linesTotal) * 100;
+                if (
+                    blockCount < distributionAmount - 1 &&  //If there is only one block left, just show its color.
+                    (currentStartPos > GRAPH_CUT_OFF_POINT || // if more blocks after the cut off point, make them one grey block
+                    (currentStartPos > GRAPH_SMALL_CHECK_STARTING_POINT && blockSize < GRAPH_SMALL_CHECK_MIN_SIZE)) // if the blocks are very small and after the "small_check_starting_point" make them one grey block
+                )
+                {//Adding "other"-section to graph
+                    GraphBlock block = new GraphBlock();
+                    block.StartValue = currentStartPos;
+                    block.EndValue = 100;
+                    block.Color = Color.FromArgb(255, 150, 150, 150);
+                    block.Name = String.Format("{0} others", distributionAmount - blockCount);
+                    block.Percentage = (1 - (double)lineCount / (double)linesTotal) * 100;
+                    block.ToolTip = string.Format("{0} others: {1:N2}%", distributionAmount - blockCount, (1 - (double)lineCount / (double)linesTotal) * 100);
+
+                    currentStartPos = block.EndValue;
+
+                    blockList.Add(block);
+                    break;
+                }
+                else
+                {
+                    GraphBlock block = new GraphBlock();
+                    block.StartValue = currentStartPos;
+                    block.EndValue = blockSize + currentStartPos;
+                    block.Color = dist.Key.Color;
+                    block.Name = dist.Key.Name;
+                    block.Percentage = blockSize;
+                    block.ToolTip = string.Format("{0}: {1:N2}%", dist.Key.Name, blockSize);
+
+                    currentStartPos = block.EndValue;
+
+                    lineCount += dist.Value;
+
+                    blockList.Add(block);
+                }
+                blockCount++;
+
+            }
+
+            return blockList;
+        }
+    }
+}
