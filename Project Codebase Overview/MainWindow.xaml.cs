226a04cd547323fad08b4257d8961c8d3342b87e
jacoberiksen1211 <jacob1211@hotmail.com>
2022-11-10 13:01:12 +0100

    ctrl Z and ctrl Y implemented

--- Project Codebase Overview/MainWindow.xaml.cs
+++ Project Codebase Overview/MainWindow.xaml.cs
@@ -23,7 +23,10 @@ using Windows.Graphics.Display;
 using Windows.Storage.Pickers;
 using Windows.UI.ViewManagement;
 using Windows.UI.WindowManagement;
+using Windows.UI.Core;
 using AppWindow = Microsoft.UI.Windowing.AppWindow;
+using Windows.System;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,6 +38,7 @@ namespace Project_Codebase_Overview
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        bool controlKeyDown = false;
         public MainWindow()
         {
             this.InitializeComponent();
@@ -54,6 +58,30 @@ namespace Project_Codebase_Overview
         public async void NavigateToManagementPage()
         {
             this.NavigationFrame.Navigate(typeof(ManagementPage));
+        }
+
+        private void NavigationFrame_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Control)
+            {
+                controlKeyDown = false;
+            }
+            else if (e.Key == VirtualKey.Z && controlKeyDown && PCOState.GetInstance().ChangeHistory.UndoAvailable)
+            {
+                PCOState.GetInstance().ChangeHistory.Undo();
+            }
+            else if (e.Key == VirtualKey.Y && controlKeyDown && PCOState.GetInstance().ChangeHistory.RedoAvailable)
+            {
+                PCOState.GetInstance().ChangeHistory.Redo();
+            }
+        }
+
+        private void NavigationFrame_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == VirtualKey.Control)
+            {
+                controlKeyDown = true;
+            }
         }
     }
 }
